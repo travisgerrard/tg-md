5ede72715d0cffb252487f7c0ecd92c78a1e406f
travisgerrard <travisgerrard@gmail.com>
2016-10-23 20:12:00 -0700

    hello world with react hashnode.com/post/react-tutorial-using-mern-stack-ciiyus9m700qqge53mer0isxz

---
+++ hello-world/webapp.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+app.use(express.static('static'));
+
+var bugData = [
+  {id: 1, priority: 'P1', status: 'Open', owner: 'Raven', title: 'App crashes on open' },
+  {id: 2, priority: 'P2', status: 'New', owner: 'Eddie', title: 'Misaligned border on panel' },
+];
+
+app.get('/api/bugs', function(req, res) {
+  res.json(bugData);
+});
+
+app.use(bodyParser.json());
+app.post('/api/bugs', function(req, res) {
+  console.log("Req body:", req.body);
+  var newBug = req.body;
+  newBug.id = bugData.length + 1;
+  bugData.push(newBug);
+  res.json(newBug);
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
