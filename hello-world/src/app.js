a4a245d421600eba7c84afe92aad5315948613f9
travisgerrard <travisgerrard@gmail.com>
2016-12-27 09:26:39 -0800

    added react Router

--- hello-world/src/app.js
+++ hello-world/src/app.js
@@ -1,9 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+import { Router, Route, Link, Redirect, browserHistory } from 'react-router'
 
 var BugList = require('./BugList');
 
+var NoMatch = React.createClass({
+  render: function() {
+    return (
+      <h2>No match for the route</h2>
+    );
+  }
+});
+
 ReactDOM.render(
-    <BugList />,
+    (
+      <Router history={browserHistory}>
+        <Route path='/bugs' component={BugList} />
+        <Redirect from="/" to="/bugs" />
+        <Route path="*" component={NoMatch} />
+      </Router>
+    ),
     document.getElementById('main')
 );
