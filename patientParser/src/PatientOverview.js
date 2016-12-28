e63a88e9311d1520b90d7fbbc5ba60bb32d315ee
Travis Gerrard <travisgerrard@gmail.com>
2016-12-28 12:38:47 -0800

    patientOverview to ES6

--- patientParser/src/PatientOverview.js
+++ patientParser/src/PatientOverview.js
@@ -1,33 +1,35 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 require('../sass/PatientOverview.scss');
 
-var PatientOverview = React.createClass({
-  decodeString: function(stringToDecode) {
+export default class PatientOverview extends React.Component {
+  decodeString(stringToDecode) {
     var decodedString = CryptoJS.AES.decrypt(stringToDecode, this.props.secretCode).toString(CryptoJS.enc.Utf8);
     return decodedString;
-  },
+  }
 
-  getInitialState() {
-      return {
-        input: this.props.patientData.input,
-        output: this.props.patientData.output,
-      }
-    },
+  constructor(props) {
+    super(props);
+    this.state = {
+      patientData: this.props.patientData
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-  handleChange: function(event) {
+  handleChange(event) {
     this.props.onUpdate(event.target, this.props.patientData._id);
+  }
 
-  },
-
-  render: function() {
+  render() {
     return(
       <div id="Overview">
         <textarea className="Overview" onChange={this.handleChange} defaultValue={(this.props.patientData.overview === undefined) ? "" : this.decodeString(this.props.patientData.overview)}/>
       </div>
     )
   }
-});
+
+}
+
 
 module.exports = PatientOverview;
