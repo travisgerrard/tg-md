eba804206afd2d099ac81fced5dfb9b0b8f29248
travisgerrard <travisgerrard@gmail.com>
2017-01-07 20:35:05 -0800

    cleaned up code in dailyTodoUpdate

--- patientParser/client/src/containers/PatientDailyTodoPage.jsx
+++ patientParser/client/src/containers/PatientDailyTodoPage.jsx
@@ -33,57 +33,10 @@ class PatientDailyTodoPage extends React.Component {
       var value = event.target.value;
       var className = event.target.className;
 
-      // Here we handle changes locally while sending changes to server
-      switch(value) {
-        case 'labsback':
-          this.props.onUpdate({className: className, trueFalse: this.state.labsback}, this.props.patientData._id);
-          (this.state.labsback === true) ? this.setState({ labsback : false }) : this.setState({ labsback : true });
-          break;
-        case 'consults':
-          this.props.onUpdate({className: className, trueFalse: this.state.consults}, this.props.patientData._id);
-          (this.state.consults === true) ? this.setState({ consults : false }) : this.setState({ consults : true });
-          break;
-        case 'andon':
-          this.props.onUpdate({className: className, trueFalse: this.state.andon}, this.props.patientData._id);
-          (this.state.andon === true) ? this.setState({ andon : false }) : this.setState({ andon : true });
-          break;
-        case 'mar':
-          this.props.onUpdate({className: className, trueFalse: this.state.mar}, this.props.patientData._id);
-          (this.state.mar === true) ? this.setState({ mar : false }) : this.setState({ mar : true });
-          break;
-        case 'ivmed':
-          this.props.onUpdate({className: className, trueFalse: this.state.ivmed}, this.props.patientData._id);
-          (this.state.ivmed === true) ? this.setState({ ivmed : false }) : this.setState({ ivmed : true });
-          break;
-        case 'amlab':
-          this.props.onUpdate({className: className, trueFalse: this.state.amlab}, this.props.patientData._id);
-          (this.state.amlab === true) ? this.setState({ amlab : false }) : this.setState({ amlab : true });
-          break;
-        case 'dispo':
-          this.props.onUpdate({className: className, trueFalse: this.state.dispo}, this.props.patientData._id);
-          (this.state.dispo === true) ? this.setState({ dispo : false }) : this.setState({ dispo : true });
-          break;
-        case 'learning':
-          this.props.onUpdate({className: className, trueFalse: this.state.learning}, this.props.patientData._id);
-          (this.state.learning === true) ? this.setState({ learning : false }) : this.setState({ learning : true });
-          break;
-        case 'seen':
-          this.props.onUpdate({className: className, trueFalse: this.state.seen}, this.props.patientData._id);
-          (this.state.seen === true) ? this.setState({ seen : false }) : this.setState({ seen : true });
-          break;
-        case 'lines':
-          this.props.onUpdate({className: className, trueFalse: this.state.lines}, this.props.patientData._id);
-          (this.state.lines === true) ? this.setState({ lines : false }) : this.setState({ lines : true });
-          break;
-        case 'foley':
-          this.props.onUpdate({className: className, trueFalse: this.state.foley}, this.props.patientData._id);
-          (this.state.foley === true) ? this.setState({ foley : false }) : this.setState({ foley : true });
-          break;
-        case 'mobility':
-          this.props.onUpdate({className: className, trueFalse: this.state.mobility}, this.props.patientData._id);
-          (this.state.mobility === true) ? this.setState({ mobility : false }) : this.setState({ mobility : true });
-          break;
-      }
+      console.log(value, className);
+
+      this.props.onUpdate({className: className, trueFalse: this.state[value]}, this.props.patientData._id); // updates on the server. Got rid of lots of code with [value]
+      (this.state[value] === true) ? this.setState({ [value] : false }) : this.setState({ [value] : true }); // updates locally.
     }
 
     render() {
