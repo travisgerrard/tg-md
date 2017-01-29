a183a1d7486ab3b725571d48b014845e68d0d205
Travis Gerrard <travisgerrard@gmail.com>
2017-01-29 14:03:47 -0800

    no longer stoped editing with double click

--- patientParser/client/src/containers/PatientFollowUpsPage.jsx
+++ patientParser/client/src/containers/PatientFollowUpsPage.jsx
@@ -42,7 +42,13 @@ class PatientFollowUpsPage extends React.Component {
     }
 
     editElement(event) {
-      this.props.onUpdate(event.target, this.props.patientData._id);
+      // event.currentTarget.dataset.id gives index of tapped item in list
+      // We can then probe if currently isEditing
+      // If already editing, will not change isEditing to false with double click
+      if (!this.state.data.followup[event.currentTarget.dataset.id].isEditing) {
+        this.props.onUpdate(event.target, this.props.patientData._id);
+      }
+
     }
 
     handleKeyPress(event) {
